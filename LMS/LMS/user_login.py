df2a5b8f6c688d2b52e25767ad02a641aed449f0
nvineng <naveen211063cse@sdbc.ac.in>
2023-12-31 09:40:40 +0530

    login and register

--- LMS/LMS/user_login.py
+++ LMS/LMS/user_login.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect,render
 from django.contrib.auth.models import User
 from django.contrib import messages
+from app.EmailBackEnd import EmailBackEnd
+from django.contrib.auth import authenticate,login,logout
 
 def REGISTER(request):
 
@@ -33,5 +35,20 @@ def REGISTER(request):
     return render(request,'registration/register.html')
 
 def Do_LOGIN(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = EmailBackEnd.authenticate(request,
+                                         username=email,
+                                         password=password)
+        
+        if user != None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Email and Password Are Invalid !')
+            return redirect('login')
+    
 
     return None
